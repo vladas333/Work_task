7099403736d20c12050aea126199b4ae11f43172
Vladas <vladas.budvytis@gmail.com>
2023-09-27 20:03:17 +0300

    Updated UnitTest test plan

--- tests/test_create_octree.py
+++ tests/test_create_octree.py
@@ -1,28 +1,16 @@
 import unittest
 from app import create_octree
-# Import the create_octree function here
-# You may also need to import other necessary libraries
 
 class TestCreateOctree(unittest.TestCase):
 
     def test_depth_zero(self):
-        # Test the base case when depth is 0
-        point_cloud = []  # Empty point cloud
+        point_cloud = []  
         min_coords = [0, 0, 0]
         max_coords = [1, 1, 1]
         depth = 0
 
         result = create_octree(point_cloud, min_coords, max_coords, depth)
-        
-        # Assert that the result is None since it should not create any nodes
         self.assertIsNone(result)
-
-    # def test_depth_one(self):
-        # Test a simple case with depth 1
-        # You can create a small point cloud and check the structure of the resulting octree
-        # Add your test case here
-
-    # Add more test cases to cover various scenarios and edge cases
 
 if __name__ == '__main__':
     unittest.main()
